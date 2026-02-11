1790bb484e555e419d2a008f1fc77a6eccd9d633
Itsuki Okuno <iokuno@c5r4s5.42tokyo.jp>
2026-02-11 15:33:50 +0900

    coloring main

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -7,7 +7,7 @@ int main(void)
 	std::string *stringPTR = &reference;
 	std::string &stringREF = reference;
 
-	std::cout << "Addresses :" << std::endl;
+	std::cout << "\x1b[38;2;160;160;160m" << "Addresses :" << "\x1b[m" << std::endl;
 	// • The memory address of the string variable.
 	// • The memory address held by stringPTR.
 	// • The memory address held by stringREF.
@@ -15,7 +15,7 @@ int main(void)
 	std::cout << std::setw(20) << stringPTR << std::endl;
 	std::cout << std::setw(20) << &stringREF << std::endl;
 
-	std::cout << "Value :" << std::endl;
+	std::cout << "\x1b[38;2;160;160;160m" << "Value" << "\x1b[m" << std::endl;
 	// • The value of the string variable.
 	// • The value pointed to by stringPTR.
 	// • The value pointed to by stringRE
